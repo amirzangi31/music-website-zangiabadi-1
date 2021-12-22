53dac73fe4efdcea675990d7599cbe14bc261b35
amirmohammad <zangiabadi1378888@gmail.com>
2021-12-22 14:30:50 +0330

    create section supper-=offer

--- dist/assets/scripts/scripts.js
+++ dist/assets/scripts/scripts.js
@@ -23,4 +23,20 @@ setInterval(() => {
 }, 5000);
 
 ///swiper js/////////////////////////////////////////////////////////////////////
+let timing = 86400;
+// let h = document.querySelector(".hour").innerHTML = 20;
 
+function setTime() {
+  if (timing == 0) return;
+  let h = Math.floor(timing / 3600);
+  let m = Math.floor((timing % 3600) / 60);
+  let s = (timing % 3600) % 60;
+  document.querySelector(".hour").innerHTML = h;
+  document.querySelector(".minutes").innerHTML = m;
+  document.querySelector(".seconds").innerHTML = s;
+}
+
+setInterval(() => {
+  timing -= 1;
+  setTime();
+}, 1000);
